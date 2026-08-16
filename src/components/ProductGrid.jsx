1906fb99b0b73299321572ee36c8cd74bb32e811
Amal cs <amalcs485@gmail.com>
2026-08-16 12:56:17 +0530

    Fix import path case sensitivity

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getProducts } from "../services/api";
-import { useInView } from "../hooks/useInView";
+import { useInView } from "../hooks/UseInView";
 import ProductCard from "./ProductCard";
 import SkeletonGrid from "./SkeletonGrid";
 
