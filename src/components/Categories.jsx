8a11c21e880af31da2e89f8473f1fe057b11f6b5
Amal cs <amalcs485@gmail.com>
2026-08-16 12:47:45 +0530

    Fix UseInView import

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getProducts } from "../services/api";
-import { useInView } from "../hooks/useInView";
+import { useInView } from "../hooks/UseInView";
 
 const CATEGORY_META = [
   { value: "smartphones", label: "Smartphones", icon: "📱" },
